9b1373a47573bbaaf55adc19592fa7f3ca162e32
jacobtheis <jtheis@nationalhomerentals.com>
2025-04-01 18:01:42 -0500

    Finally fixed the nav bar and the styles

--- apps/personas/urls.py
+++ apps/personas/urls.py
@@ -1,9 +1,9 @@
-    from django.urls import path
-    from . import views
+from django.urls import path
+from . import views
 
-    app_name = 'personas'
+app_name = 'personas'
 
-    urlpatterns = [
-        path('', views.PersonasView.as_view(), name='index'),
-        # Add other persona-related URLs here (e.g., create, detail, update, delete)
-    ]
+urlpatterns = [
+    path('', views.PersonasView.as_view(), name='index'),
+    # Add other persona-related URLs here (e.g., create, detail, update, delete)
+]
